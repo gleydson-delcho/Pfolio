c8371e9271613994946bd9535e53758f164ccaa4
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-08-03 20:49:32 -0300

    Add the toggle parts for new pages in this component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import Link from 'next/link';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa'
 
 import styles from './styles.module.scss';
+import { useState } from 'react';
 
-const Navbar: React.FC  = () => {
+const Navbar: React.FC = () => {
 
-        return(
-        
-            <div className={styles.navContainer} >
-                <Link href='/' ><a><h2>Inicial</h2></a></Link>
-                <Link href="/projects" ><a><h2>Projetos</h2></a></Link>
-                <Link href="/academy" ><a><h2>Acadêmico</h2></a></Link>
-                <Link href="/aboutme" ><a><h2>Sobre mim</h2></a></Link>
+    const [toggle, setToggle] = useState(false)
+
+    const handleToggle = () => {
+        if (toggle === false) {
+            setToggle(true);
+        } else {
+            setToggle(false)
+        }
+    }
+
+    return (
+
+        <div className={styles.navContainer} >
+            <Link href='/' ><a><h2>Inicial</h2></a></Link>
+            <Link href="/projects" ><a><h2>Projetos</h2></a></Link>
+            <div className={styles.navBlock}>
+                <span>
+                    Tecnologias
+                    <button onClick={handleToggle}>
+                        {toggle ? <FaAngleUp /> : <FaAngleDown />}
+                    </button>
+                </span>
+                {
+                    toggle &&
+                    <div className={styles.blockHidden}>
+                        <Link href="/react">React</Link>
+                        <Link href="/next">Next</Link>
+                        <Link href="/java">Java</Link>
+                        <Link href="/node">NodeJs</Link>
+                    </div>
+                }
             </div>
-        
+            <Link href="/academy" ><a><h2>Acadêmico</h2></a></Link>
+            <Link href="/aboutme" ><a><h2>Sobre mim</h2></a></Link>
+        </div>
+
     )
 };
 
